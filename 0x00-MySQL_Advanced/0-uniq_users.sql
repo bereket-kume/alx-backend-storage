257a0832cc8f14cfa009188a19c3ed1ed623efc0
bereket-kume <bereketkume@gmail.com>
2024-08-08 20:34:16 +0300

    done

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
--- Script that create a tablr users
-CREATE TABLE users (
+-- Script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
