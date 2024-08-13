0ec451a8c7dc3764022e6b069d5c8ae14a7871ae
bereket-kume <bereketkume@gmail.com>
2024-08-13 18:14:17 +0300

    done

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+List all documents in python
+"""
+
 
 def list_all(mongo_collection):
     """
         python function that lists all documents in a collection
+
+        param mongo_collection
+        return list
     """
     data = mongo_collection.find()
     if not data:
