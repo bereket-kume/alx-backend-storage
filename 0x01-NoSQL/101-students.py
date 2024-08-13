be8d1d7e6b59e21941daee545a120662fe0bc3c1
bereket-kume <bereketkume@gmail.com>
2024-08-13 22:26:41 +0300

    done

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def top_students(mongo_collection):
+    """
+    function that returns all student sorted by average score
+
+    return the list of student from to top to lower
+    """
+
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score'
+                        },
+                    },
+                    'topics': 1
+                },
+            },
+            {
+                '$sort': {'averageScore': -1}
+            }
+        ]
+    )
+    return students
